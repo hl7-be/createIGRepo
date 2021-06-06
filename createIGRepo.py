3cd5f90cd1f3143faa8c8c3438cb6bd82f34df16
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2021-06-06 16:28:54 +0200

    Update createIGRepo.py

--- createIGRepo.py
+++ createIGRepo.py
@@ -5,7 +5,6 @@ from requests.auth import HTTPBasicAuth
 import os
 from pprint import pprint
 
-
 # Don't touch this
 baseUrl = 'https://api.github.com/'
 headers1 = {'Accept': 'application/vnd.github.v3+json'}
@@ -19,26 +18,26 @@ headers3 = {'Accept': 'application/vnd.github.inertia-preview+json'}
 
 
 # Set these variables to true or false, or comment them out for prompting
-#create_project   = False
-#create_repo      = False
-#create_readme    = False
-#create_gitignore = False
-#create_columns   = False
-#create_hook      = False
-#hook_active      = False
+create_project   = True
+create_repo      = True
+create_readme    = True
+create_gitignore = True
+create_columns   = True
+create_hook      = True
+hook_active      = False
 
 
 ## this is the repository that is going to be created
 user = 'costateixeira'
 token = ''  # Add your secret token here
-email = 'mail@openhie.org'
-owner = 'openhie'
-repo = 'case-reporting'
+email = 'mail@hl7-be.org'
+owner = 'costateixeira'
+repo = 'ig_composition'
 #ownerType = ''
 
 ## this is the template repository that is going to be cloned
-template_owner = 'openhie'
-template_repo = 'empty-fhir-ig'
+template_owner = 'hl7-be'
+template_repo = 'empty-ig'
 
 #####################################################################################################################
 #####################################     Don't change beyond this point    #########################################
@@ -47,7 +46,7 @@ template_repo = 'empty-fhir-ig'
 
 if (user==''):
   user = input("Username (to login): ")   
-print("username: "+owner+"\n")
+print("username: "+user+"\n")
 
 if (token==''):
   token = input("Token or password is empty. Add your password or token: ")   
@@ -57,7 +56,7 @@ auth = HTTPBasicAuth(user, token)
 
 if (email==''):
   email = input("email of repository owner: ")   
-print("Owner email: "+owner+"\n")
+print("Owner email: "+email+"\n")
 
 if (owner==''):
   owner = input("Repository owner (github handle to create repository): ")   
@@ -103,7 +102,7 @@ if (template_repo==''):
 print("Using template repository: "+template_repo+"\n")
 
 r = requests.get(baseUrl+'repos/'+template_owner+'/'+template_repo, headers=headers2, auth=auth)
-# TO DO: Check if template repo exists? abort if not
+
 if (r.status_code == 200):
   print("Template exists: "+template_owner+"/"+template_repo+"\n")
 else:
@@ -162,7 +161,7 @@ except:
 
 ########### POST repo
 create_repo_url=baseUrl+'repos/'+template_owner+'/'+template_repo+'/generate'
-repo_create_body ={'owner': owner,'name': repo, 'description': repo + "Implementation Guide" }
+repo_create_body ={'owner': owner,'name': repo, 'description': repo + "Implementation Guide", 'include_all_branches':True, 'private': repo_private }
 
 if create_repo:
     r = requests.post(create_repo_url, data=json.dumps(repo_create_body), headers=headers2, auth=auth)
@@ -187,7 +186,7 @@ if (r.status_code == 200):
 if create_gitignore:
     gitignore = "# Temporary folders #\n#####################\ntemp\ntemplate\noutput\nfsh-generated\n\n# Don't commit this because it's so large #\n###########################################\n/input-cache/publisher.jar\n\n# Windows generated files #\n###########################\nThumbs.db\n\n# OS generated files #\n######################\n.DS_Store\n.DS_Store?\n\n# backup files #\n################\n*.bak"
     gitignore = base64.b64encode(gitignore.encode('utf-8'))
-    create_gitignore_body = {'message': "create gitignore", 'committer': {'name': owner,'email': email}, "sha": gitignore_sha, "content": "IyBUZW1wb3JhcnkgZm9sZGVycyAjCiMjIyMjIyMjIyMjIyMjIyMjIyMjIwp0ZW1wCnRlbXBsYXRlCm91dHB1dApmc2gtZ2VuZXJhdGVkLwoKIyBEb24ndCBjb21taXQgdGhpcyBiZWNhdXNlIGl0J3Mgc28gbGFyZ2UgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCi9pbnB1dC1jYWNoZS9vcmcuaGw3LmZoaXIucHVibGlzaGVyLmphcgovaW5wdXQtY2FjaGUvcHVibGlzaGVyLmphcgoKIyBXaW5kb3dzIGdlbmVyYXRlZCBmaWxlcyAjCiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwpUaHVtYnMuZGIKCiMgT1MgZ2VuZXJhdGVkIGZpbGVzICMKIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwouRFNfU3RvcmUKLkRTX1N0b3JlPwoKIyBiYWNrdXAgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjCiouYmFr"}
+    create_gitignore_body = {'message': "create gitignore", 'committer': {'name': owner,'email': email}, "sha": gitignore_sha, "content": "IyBUZW1wb3JhcnkgZm9sZGVycyAjCiMjIyMjIyMjIyMjIyMjIyMjIyMjIwp0ZW1wCnRlbXBsYXRlCm91dHB1dAppbnB1dC1jYWNoZQpmc2gtZ2VuZXJhdGVkCgojIERvbid0IGNvbW1pdCB0aGlzIGJlY2F1c2UgaXQncyBzbyBsYXJnZSAjCiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKL2lucHV0LWNhY2hlL3B1Ymxpc2hlci5qYXIKCiMgV2luZG93cyBnZW5lcmF0ZWQgZmlsZXMgIwojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKVGh1bWJzLmRiCgojIE9TIGdlbmVyYXRlZCBmaWxlcyAjCiMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKLkRTX1N0b3JlCi5EU19TdG9yZT8KCiMgYmFja3VwIGZpbGVzICMKIyMjIyMjIyMjIyMjIyMjIwoqLmJhawo="}
     r = requests.put(create_file_url+".gitignore", data=json.dumps(create_gitignore_body), headers=headers1, auth=auth)
     if (r.status_code == 200):
         response=r.json()
@@ -280,4 +279,21 @@ if create_hook:
 # * Programmatically Setup automation
 # * Also create feedback form  and data/features.xml
 # * Add metadata for publication (when that is decided and working)
+
+
+########### POST workflow
+if create_workflow:
+    
+    create_gitactions_body = {'message': "create gitignore", 'committer': {'name': owner,'email': email}, "sha": gitignore_sha, "content": "IyBUaGlzIGlzIGEgc2ltcGxlIHdvcmtmbG93IHRoYXQgcnVucyB0aGUgcHVibGlzaGVyIGFuZCBjb3BpZXMgdGhlIG91dHB1dCB0byBodHRwczovLzxvd25lcj4uZ2l0aHViLmlvLzxyZXBvPi9pbmRleC5odG1sCiMgQmFzZWQgb24gdGhlIGluc3RydWN0aW9ucyBmcm9tIEVsbGlvdCBTaWx2ZXIsIGF2YWlsYWJsZSBmcm9tOiBodHRwczovL3d3dy5hcmdlbnRpeGluZm8uY29tL2FyY2hpdmVzLzE1NgoKIyBNYWtlIHN1cmUgeW91ciByZXBvIGhhcyBhIGJyYW5jaCBjYWxsZWQgZ2gtcGFnZXMKCm5hbWU6IENJCgojIENvbnRyb2xzIHdoZW4gdGhlIGFjdGlvbiB3aWxsIHJ1bi4gCm9uOgogICMgVHJpZ2dlcnMgdGhlIHdvcmtmbG93IG9uIHB1c2ggb3IgcHVsbCByZXF1ZXN0IGV2ZW50cyBidXQgb25seSBmb3IgdGhlIG1haW4gYnJhbmNoCiAgcHVzaDoKICAgIGJyYW5jaGVzOiBbIG1haW4gXQogIHB1bGxfcmVxdWVzdDoKICAgIGJyYW5jaGVzOiBbIG1haW4gXQoKICAjIEFsbG93cyB5b3UgdG8gcnVuIHRoaXMgd29ya2Zsb3cgbWFudWFsbHkgZnJvbSB0aGUgQWN0aW9ucyB0YWIKICB3b3JrZmxvd19kaXNwYXRjaDoKCiMgQSB3b3JrZmxvdyBydW4gaXMgbWFkZSB1cCBvZiBvbmUgb3IgbW9yZSBqb2JzIHRoYXQgY2FuIHJ1biBzZXF1ZW50aWFsbHkgb3IgaW4gcGFyYWxsZWwKam9iczoKICAjIFRoaXMgd29ya2Zsb3cgY29udGFpbnMgYSBzaW5nbGUgam9iIGNhbGxlZCAiYnVpbGQiCiAgYnVpbGQ6CiAgICAjIFRoZSB0eXBlIG9mIHJ1bm5lciB0aGF0IHRoZSBqb2Igd2lsbCBydW4gb24KICAgIHJ1bnMtb246IHVidW50dS1sYXRlc3QKCiAgICAjIFN0ZXBzIHJlcHJlc2VudCBhIHNlcXVlbmNlIG9mIHRhc2tzIHRoYXQgd2lsbCBiZSBleGVjdXRlZCBhcyBwYXJ0IG9mIHRoZSBqb2IKICAgIHN0ZXBzOgogICAgICAjIENoZWNrcy1vdXQgeW91ciByZXBvc2l0b3J5IHVuZGVyICRHSVRIVUJfV09SS1NQQUNFLCBzbyB5b3VyIGpvYiBjYW4gYWNjZXNzIGl0CiAgICAgIC0gdXNlczogYWN0aW9ucy9jaGVja291dEB2MgogICAgICAtIG5hbWU6IFVwZGF0ZSB0aGUgaW1hZ2UgdG8gdGhlIGxhdGVzdCBwdWJsaXNoZXIKICAgICAgICB1c2VzOiBkb2NrZXI6Ly9obDdmaGlyL2lnLXB1Ymxpc2hlci1iYXNlOmxhdGVzdAogICAgICAgIHdpdGg6CiAgICAgICAgICAjIEdldCB0aGUgbGF0ZXN0IHB1Ymxpc2hlciAtIGRvbid0IHJ1biB0aGUgYmF0Y2ggc2NyaXB0IGJ1dCBydW4gdGhlIGxpbmUgZGlyZWN0bHkKICAgICAgICAgIGFyZ3M6IGN1cmwgLUwgaHR0cHM6Ly9naXRodWIuY29tL0hMNy9maGlyLWlnLXB1Ymxpc2hlci9yZWxlYXNlcy9sYXRlc3QvZG93bmxvYWQvcHVibGlzaGVyLmphciAtbyAuL2lucHV0LWNhY2hlL3B1Ymxpc2hlci5qYXIgLS1jcmVhdGUtZGlycwoKICAgICAgLSBuYW1lOiBSdW4gdGhlIElHIHB1Ymxpc2hlcgogICAgICAgIHVzZXM6IGRvY2tlcjovL2hsN2ZoaXIvaWctcHVibGlzaGVyLWJhc2U6bGF0ZXN0CiAgICAgICAgd2l0aDoKICAgICAgICAgICMgUnVuIHRoZSBwdWJsaXNoZXIgLSBkb24ndCBydW4gdGhlIGJhdGNoIHNjcmlwdCBidXQgcnVuIHRoZSBsaW5lIGRpcmVjdGx5CiAgICAgICAgICBhcmdzOiBqYXZhIC1qYXIgLi9pbnB1dC1jYWNoZS9wdWJsaXNoZXIuamFyIHB1Ymxpc2hlciAtaWcgLgoKICAgICAgLSBuYW1lOiBEZXBsb3kKICAgICAgICB1c2VzOiBwZWFjZWlyaXMvYWN0aW9ucy1naC1wYWdlc0B2MwogICAgICAgIHdpdGg6CiAgICAgICAgICBnaXRodWJfdG9rZW46ICR7eyBzZWNyZXRzLkdJVEhVQl9UT0tFTiB9fQogICAgICAgICAgcHVibGlzaF9kaXI6IC4vb3V0cHV0"}
+    r = requests.put(create_file_url+".github/worflows/main.yml", data=json.dumps(create_gitactions_body), headers=headers1, auth=auth)
+    if (r.status_code == 200):
+        response=r.json()
+        print('.github workflow Created')
+
+
+
+clone = (input("Create folder? "+repo+" and clone? (Y/N): ").upper() == 'Y')
+if clone:
+  os.mkdir("repo")
+  os.system("git clone https://github.com/"+owner+"/"+repo)
 
